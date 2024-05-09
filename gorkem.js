b075a51957c00097a07f8feca7a7f23519c9b560
gorkem <113816295+gorkem1940@users.noreply.github.com>
2024-05-09 09:38:19 +0300

    gorkem.js

--- gorkem.js
+++ gorkem.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
 
 function veriCekVeYaz() {
-    var url = 'http://88.250.204.138:4545/status.xml'; 
+    var url = 'http://88.250.204.138:4545/status.xml'; //bu siteden veriyi al 
     fetch(url)
         .then(response => response.text())
         .then(data => {
             
-            fs.appendFile('status.xml', data + '\n', function (err) {
+            fs.appendFile('status.xml', data + '\n', function (err) { // status.xml yazan yere verinin yazılacağı dosya ismini yazalım
                 if (err) throw err;
                 console.log('Veri başarıyla yazıldı.');
             });
